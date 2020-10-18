7a49bf4ab62642a53fd2bb948d4f41a796cd0ab8
himanshu <himanshugharat44@gmail.com>
2020-10-18 07:30:39 -0500

    refactor: the quantity to return it selected value to ts

--- src/app/components/quantity/quantity.component.ts
+++ src/app/components/quantity/quantity.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 interface quantity {
   value: string;
@@ -13,43 +13,43 @@ interface quantity {
 export class QuantityComponent implements OnInit {
   form: FormGroup;
   quantity: quantity[] = [
-    {value: 'length-0', viewValue: 'Feet'},
-      {value: 'length-1', viewValue: 'Inch'},
-      {value: 'length-2', viewValue: 'Yard'},
-      {value: 'length-3', viewValue: 'Centimeter'},
+    { value: 'length-0', viewValue: 'Feet' },
+    { value: 'length-1', viewValue: 'Inch' },
+    { value: 'length-2', viewValue: 'Yard' },
+    { value: 'length-3', viewValue: 'Centimeter' },
   ];
   selectedFood = this.quantity[0].value;
 
-  constructor() {} 
+  constructor() { }
 
   ngOnInit(): void {
-  
+
   }
- logMg(val){
-   if(val == "length"){
-    quantity: this.quantity = [
-      {value: 'length-0', viewValue: 'Feet'},
-      {value: 'length-1', viewValue: 'Inch'},
-      {value: 'length-2', viewValue: 'Yard'},
-      {value: 'length-3', viewValue: 'Centimeter'},
-    ];
-    this.selectedFood = this.quantity[0].value;
-   }
-   if(val == "volume"){
-    quantity: this.quantity = [
-      {value: 'volume-0', viewValue: 'Litre'},
-      {value: 'volume-1', viewValue: 'Gallon'},
-    ];
-    this.selectedFood = this.quantity[0].value;
-   }
-   if(val == "temperature"){
-    quantity: this.quantity = [
-      {value: 'temperature-0', viewValue: 'Celsius'},
-      {value: 'temperature-1', viewValue: 'Fahrenheit'},
-    ];
-    this.selectedFood = this.quantity[0].value;
-   }
-   
- }
+  logMg(val) {
+    if (val == "length") {
+      this.quantity = [
+        { value: 'length-0', viewValue: 'Feet' },
+        { value: 'length-1', viewValue: 'Inch' },
+        { value: 'length-2', viewValue: 'Yard' },
+        { value: 'length-3', viewValue: 'Centimeter' },
+      ];
+      this.selectedFood = this.quantity[0].value;
+    }
+    if (val == "volume") {
+      this.quantity = [
+        { value: 'volume-0', viewValue: 'Litre' },
+        { value: 'volume-1', viewValue: 'Gallon' },
+      ];
+      this.selectedFood = this.quantity[0].value;
+    }
+    if (val == "temperature") {
+      this.quantity = [
+        { value: 'temperature-0', viewValue: 'Celsius' },
+        { value: 'temperature-1', viewValue: 'Fahrenheit' },
+      ];
+      this.selectedFood = this.quantity[0].value;
+    }
+
+  }
 
 }
