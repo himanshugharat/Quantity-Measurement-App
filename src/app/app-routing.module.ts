ed1d83551868c9614a1eff1724a07067315f069f
himanshu <himanshugharat44@gmail.com>
2020-10-19 11:00:17 -0500

    refactor: the selector used to change the to value

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { QuantityComponent } from './components/quantity/quantity.component';
 
-const routes: Routes = [{path:'header',component:HeaderComponent},{path:'home',component:HomeComponent},{path:'quantity',component:QuantityComponent}];
+const routes: Routes = [{ path: 'home',component:HomeComponent ,
+children:[{path:'header',component:HeaderComponent,children:[{path:'quantity',component:QuantityComponent}]}]}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents=[HeaderComponent,HomeComponent,QuantityComponent]
+export const routingComponents = [ HeaderComponent,QuantityComponent,HomeComponent ]
--- src/app/components/quantity/quantity.component.ts
+++ src/app/components/quantity/quantity.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 interface quantity {
   value: string;
   viewValue: string;
+  convert: number;
 }
 @Component({
   selector: 'app-quantity',
@@ -11,14 +12,19 @@ interface quantity {
   styleUrls: ['./quantity.component.scss']
 })
 export class QuantityComponent implements OnInit {
+  value1: number
+  value2: number
+  content: string
+  input1: number
+  input2: number
   form: FormGroup;
   quantity: quantity[] = [
-    { value: 'length-0', viewValue: 'Feet' },
-    { value: 'length-1', viewValue: 'Inch' },
-    { value: 'length-2', viewValue: 'Yard' },
-    { value: 'length-3', viewValue: 'Centimeter' },
+    { value: 'length-0', viewValue: 'Feet', convert: 12 },
+    { value: 'length-1', viewValue: 'Inch', convert: 1 },
+    { value: 'length-2', viewValue: 'Yard', convert: 36 },
+    { value: 'length-3', viewValue: 'Centimeter', convert: 0.4 },
   ];
-  selectedFood = this.quantity[0].value;
+  selectedQuantity = this.quantity[0].value;
 
   constructor() { }
 
@@ -28,28 +34,48 @@ export class QuantityComponent implements OnInit {
   logMg(val) {
     if (val == "length") {
       this.quantity = [
-        { value: 'length-0', viewValue: 'Feet' },
-        { value: 'length-1', viewValue: 'Inch' },
-        { value: 'length-2', viewValue: 'Yard' },
-        { value: 'length-3', viewValue: 'Centimeter' },
+        { value: 'length-0', viewValue: 'Feet', convert: 12 },
+        { value: 'length-1', viewValue: 'Inch', convert: 1 },
+        { value: 'length-2', viewValue: 'Yard', convert: 36 },
+        { value: 'length-3', viewValue: 'Centimeter', convert: 0.4 },
       ];
-      this.selectedFood = this.quantity[0].value;
+      this.selectedQuantity = this.quantity[0].value;
     }
     if (val == "volume") {
       this.quantity = [
-        { value: 'volume-0', viewValue: 'Litre' },
-        { value: 'volume-1', viewValue: 'Gallon' },
+        { value: 'volume-0', viewValue: 'Litre', convert: 1 },
+        { value: 'volume-1', viewValue: 'Gallon', convert: 3.78 },
       ];
-      this.selectedFood = this.quantity[0].value;
+      this.selectedQuantity = this.quantity[0].value;
     }
     if (val == "temperature") {
       this.quantity = [
-        { value: 'temperature-0', viewValue: 'Celsius' },
-        { value: 'temperature-1', viewValue: 'Fahrenheit' },
+        { value: 'temperature-0', viewValue: 'Celsius', convert: 0 },
+        { value: 'temperature-1', viewValue: 'Fahrenheit', convert: 0 },
       ];
-      this.selectedFood = this.quantity[0].value;
+      this.selectedQuantity = this.quantity[0].value;
     }
 
   }
+  selectChangeFrom(event) {
+    this.quantity.forEach(element => {
+      if (element.value == event) {
+        this.value1 = element.convert
+      }
+    });
+    console.log(this.value1)
 
+  }
+  selectChangeTo(event) {
+    this.quantity.forEach(element => {
+      if (element.value == event) {
+        this.value2 = element.convert
+      }
+    });
+    console.log(this.value2)
+  }
+  fromValue(event) {
+    this.input1=event.target.value
+  }
+  
 }
