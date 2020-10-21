ccde6cc6ea2795a2b3be8d139bbb4881dea03b39
himanshu <himanshugharat44@gmail.com>
2020-10-21 10:26:04 -0500

    sltyle: the quantity and the header file

--- src/app/components/quantity/quantity.component.ts
+++ src/app/components/quantity/quantity.component.ts
@@ -5,7 +5,7 @@ interface quantity {
   value: string;
   viewValue: string;
   convert: number;
-  place: number;
+  typeNo: number;
 }
 
 @Component({
@@ -14,8 +14,6 @@ interface quantity {
   styleUrls: ['./quantity.component.scss']
 })
 export class QuantityComponent implements OnInit {
-  
-  
   valueFrom: number
   valueTo: number
   content: string
@@ -24,19 +22,20 @@ export class QuantityComponent implements OnInit {
   form: FormGroup;
   isDisable: number;
   tempActiveType: boolean;
-  quantity:quantity[]=SampleJson.length;
+  quantity: quantity[] = SampleJson.length;
   selectedQuantity = this.quantity[0].value;
   active: boolean
 
-  constructor() {console.log(SampleJson); }
+  constructor() { console.log(SampleJson); }
 
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void { }
+  /**
+   * @description: to find the selected toggle and assign selector
+   * @param val 
+   */
   getToggelValue(val) {
     if (val == "length") {
-       this.quantity = SampleJson.length;
+      this.quantity = SampleJson.length;
       this.selectedQuantity = this.quantity[0].value;
     }
     if (val == "volume") {
@@ -48,6 +47,11 @@ export class QuantityComponent implements OnInit {
       this.selectedQuantity = this.quantity[0].value;
     }
   }
+
+  /**
+   * @description: get which selector is selected from-selector
+   * @param event 
+   */
   selectChangeFrom(event) {
     console.log(event)
     console.log(`${this.tempActiveType}before`)
@@ -55,23 +59,25 @@ export class QuantityComponent implements OnInit {
       if (element.value == event) {
         if (event == 'temperature-0') {
           this.active = true
-          this.tempActiveType = true;
-
+          this.tempActiveType = true
         }
         else if (event == 'temperature-1') {
           this.active = true
-          this.tempActiveType = false;
-
+          this.tempActiveType = false
         } else {
           this.active = false
         }
         console.log(`${this.tempActiveType}after`)
         this.valueFrom = element.convert
-        this.isDisable=element.place
+        this.isDisable = element.typeNo
       }
     });
   }
 
+  /**
+   * @description: get which selector is selected to-selector
+   * @param event 
+   */
   selectChangeTo(event) {
     this.quantity.forEach(element => {
       if (element.value == event) {
@@ -79,25 +85,30 @@ export class QuantityComponent implements OnInit {
       }
     });
   }
+
+  /**
+   * @description: get input value from-field
+   * @param event 
+   */
   fromValue(event) {
     this.inputFrom = event.target.value
   }
 
+  /**
+   * @description: convert the quanatity from one type to another
+   * @returns: converted value
+   */
   ConvertedValue() {
     if (this.active) {
       if (this.tempActiveType === true) {
         return (this.inputFrom - 32) * 5 / 9
-        //return (this.inputFrom*9/5)+32
       }
       else if (this.tempActiveType === false) {
-        //return (this.inputFrom-32)*5/9
         return (this.inputFrom * 9 / 5) + 32
       }
     }
     else {
       return (this.inputFrom * this.valueFrom) / this.valueTo
     }
-
   }
-
 }
